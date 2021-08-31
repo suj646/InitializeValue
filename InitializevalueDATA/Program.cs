bb8214cbb0fa1146aa7c1fcf5c324ce2c3529f06
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:30:36 +0530

    Initialize Value

---
+++ InitializevalueDATA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InitializevalueDATA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i = 89;
+            short s = 56;
+            long l = 4564;
+            double d = 8.3586745d;
+            float f = 3.7330645f;
+            decimal dec = 389.5m;
+            Console.WriteLine("integer: " + i);
+            Console.WriteLine("short: " + s);
+            Console.WriteLine("long: " + l);
+            Console.WriteLine("float: " + f);
+            Console.WriteLine("double: " + d);
+            Console.WriteLine("decimal: " + dec);
+            
+        }
+    }
+    
+}
